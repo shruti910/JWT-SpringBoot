764f55cf5fd352500048eee3d1872881797ea7ac
shruti910 <shruti.lovelyyy@gmail.com>
2021-01-24 13:31:14 +0530

    initial commit

---
+++ src/main/java/com/security/auth/jwtdemo/controller/AppController.java
@@ -0,0 +1,57 @@
+package com.security.auth.jwtdemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.security.auth.jwtdemo.JwtUtil.JwtUtil;
+import com.security.auth.jwtdemo.model.AuthenticationRequest;
+import com.security.auth.jwtdemo.model.AuthenticationResponse;
+import com.security.auth.jwtdemo.services.MyUserDetailsService;
+
+@RestController
+public class AppController {
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtUtil jwtTokenUtil;
+
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+	
+	@GetMapping("/message")
+	public String getMsg() {
+		return "Hello World";
+	}
+	
+	@PostMapping("/authenticate")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+			);
+		}
+		catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username or password", e);
+		}
+
+
+		final UserDetails userDetails = userDetailsService
+				.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+	}
+
+}
